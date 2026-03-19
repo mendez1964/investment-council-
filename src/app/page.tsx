c2a84e70d9d28f03c3ac79a68c41fc4a419d2325
Dagoberto Mendez <mendezdag@gmail.com>
2026-03-18 23:21:07 -0400

    Add Next.js pages

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Investment Council</h1>
+      <p>Platform coming soon.</p>
+    </main>
+  )
+}
